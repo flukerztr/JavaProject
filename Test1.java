4f80254c0121f0f4af7ed2f362d31d989cc0ba17
flukerztr <flukerztr@gmail.com>
2024-07-12 11:04:26 +0700

    This is 3rd status

--- Test1.java
+++ Test1.java
@@ -6,5 +6,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("A cat is red");
+        System.out.println("A cat is angry");
     }
 }
